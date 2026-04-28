78b69802b4a8c91ffa581a38879e71e8eb293693
Suraj Suryawanshi <surajsuryawanshi2424@gmail.com>
2026-04-29 00:05:34 +0530

    feat(mock): add Leetcode directory and backend documentation

---
+++ Leetcode/Find_Eventual_Safe_States.java
@@ -0,0 +1,16 @@
+package Leetcode;
+import java.util.*;
+
+class Solution {
+    // Your Solution class code here
+    
+}
+
+public class Find_Eventual_Safe_States {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Your code here
+        Solution sol = new Solution();
+        sc.close();
+    }
+}
---
+++ backend.md
@@ -0,0 +1,11 @@
+# BACKEND
+
+## API Design
+
+### What is Idempotency?
+
+- In simpler terms, an operation is idempotent if executing it once has the same effect as executing it multiple times.
+
+### Have you ever handled Pagination/Filtering
+
+- Yes, i did
